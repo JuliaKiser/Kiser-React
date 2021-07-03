c693d6ceb33e4c26d5da6b98ca5ed492170ccd06
J_Kiser <jdkiser228@gmail.com>
2021-07-02 20:47:41 -0400

    adding about me component

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,9 +1,32 @@
+import styles from "./about.module.css";
+import PageHeader from "../PageHeader/PageHeader";
+import me from "../../images/jkiser.JPG"
+
 const about = () => {
-    return (
-        <div>
-            <h1>About Me</h1>
+  return (
+    <div className={styles.AboutMe}>
+      <PageHeader title={"About Me"} />
+      <div className={styles.container}>
+        <div className={styles.text}>
+          <h2>Hello, I am Julia Kiser</h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
         </div>
-    )
-}
+        <div className={styles.photo}>
+            <img className={styles.me} src={me} alt="me"></img>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default about;
--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,13 +1,15 @@
 import styles from "./home.module.css";
+import gif from "../../images/PWGenerator.gif"
 
 const home = () => {
     return (
         <div className={styles.home}>
             <div className={styles.container}>
-                <h1 className={styles.Hello}>Hello,world</h1>
-                <h1>Welcome to my website</h1>
+                <h1 className={styles.Hello}>Welcome!</h1>
+                <h2>Check out my portfolio</h2>
                 
             </div>
+            <img className={styles.gif} src={gif} alt="gif"></img>
         </div>
     )
 }
