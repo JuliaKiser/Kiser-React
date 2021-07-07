cd555874a9a88cf6d62f1da8dee8444a2890ac1e
J_Kiser <jdkiser228@gmail.com>
2021-07-07 00:18:28 -0400

    working on project section

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -6,8 +6,8 @@ import TeamGen from "../../images/TeamGen.gif";
 
 const projectClip = (url, thumbnail, description) => {
   return (
-    <div className={styles.ClipContainer}>
-      <div className={styles.thumbnail}>
+    <div className={styles.VideoContainer}>
+      <div className={styles.Thumbnail}>
         <img src={thumbnail} alt="thumbail"></img>
       </div>
       <div className={styles.Text}>
@@ -17,18 +17,18 @@ const projectClip = (url, thumbnail, description) => {
   );
 };
 
-const marvelProjectDescription = `text text text`;
+const marvelProjectDescription = `Marvel Eye takes the user through an interactive journey to select and review some of Marvels most popular characters, watch movie trailers and explore marvels entire character database.`;
 
-const blogProjectDescription = `text text text`;
+const blogProjectDescription = `The MVC Tech Blog is designed to allow the user to sign up and login in to essentially a blog post application. You can create new posts, edit/delete post or comment on posts that have already been made.`;
 
-const teamProjectDescription = `text text text`;
+const teamProjectDescription = `This web application allows the user to create, modify and delete notes. It utilizes Express.js backend that saves/retrieves files from a JSON file.`;
 
 const project = () => {
   return (
     <div className={styles.project}>
       <PageHeader title={"PROJECTS"} />
       <div className={styles.projectcontent}>
-        <div className={styles.paragraph}>
+        <div className={styles.Paragraph}>
           <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
@@ -37,10 +37,22 @@ const project = () => {
             only five centuries, but also the leap into electronic typesetting,
             remaining essentially unchanged.
           </p>
-          <div className={styles.gifs}></div>
-          {projectClip("https://github.com/JuliaKiser/Marvel.Eye", Marvel, marvelProjectDescription)}
-          {projectClip("https://github.com/JuliaKiser/MVC_Tech_Blog", Blog, blogProjectDescription)}
-          {projectClip("https://github.com/JuliaKiser/Team.Generator", TeamGen, teamProjectDescription)}
+          <div className={styles.Videos}></div>
+          {projectClip(
+            "https://github.com/JuliaKiser/Marvel.Eye",
+            Marvel,
+            marvelProjectDescription
+          )}
+          {projectClip(
+            "https://github.com/JuliaKiser/MVC_Tech_Blog",
+            Blog,
+            blogProjectDescription
+          )}
+          {projectClip(
+            "https://github.com/JuliaKiser/Team.Generator",
+            TeamGen,
+            teamProjectDescription
+          )}
         </div>
       </div>
     </div>
