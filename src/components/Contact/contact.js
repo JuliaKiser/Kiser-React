49063c54cfa5cd088f641d047e34699da2d894f5
J_Kiser <jdkiser228@gmail.com>
2021-07-07 14:10:04 -0400

    finishing contact section

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,7 +1,22 @@
+import { FaPhoneAlt,FaEnvelopeOpenText,FaGithubSquare, FaLinkedin } from "react-icons/fa";
+import styles from './contact.module.css';
+
+const handleURL = (url) => {
+    return event => window.open(url, "_blank")
+}
 const Contact = () => {
     return (
-        <div>
-            <h1>Contact Me</h1>
+        <div className={styles.Contact}>
+            <div className={styles.ContactIcons}>
+            <FaPhoneAlt color='black' size='40px' style={{padding: '1%'}}
+            onClick={handleURL('https://hihello.me/p/bcb53d30-d5ed-45dd-bee1-00cddb3e4fd5')} />
+            <FaEnvelopeOpenText color='black' size='40px' style={{padding: '1%'}}
+            onClick={handleURL('mailto:jdkiser228@gmail.com')} />
+            <FaGithubSquare color='black' size='40px' style={{padding: '1%'}}
+            onClick={handleURL('https://github.com/JuliaKiser')} />
+            <FaLinkedin color='black' size='40px' style={{padding: '1%'}}
+            onClick={handleURL('https://www.linkedin.com/in/julia-kiser-mba-09a45373/')} />
+        </div>
         </div>
     )
 }
